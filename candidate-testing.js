3528669c6f0ff84eecdeabde00a5d12c0bf2d50e
caitlinrsmith <catie12smith@gmail.com>
2024-04-01 23:25:06 -0500

    all 17 tests passed

--- candidate-testing.js
+++ candidate-testing.js
@@ -11,9 +11,15 @@ let correctAnswer = "Sally Ride";
 let candidateAnswer = "";
 
 //TODO: Variables for Part 2
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let questions = [
+  "Who was the first American woman in space? ",
+  "True or false: 5 kilometer == 5000 meters? ",
+  "(5 + 3)/2 * 10 = ? ",
+  "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ",
+  "What is the minimum crew size for the ISS? ",
+];
+let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
+let candidateAnswers = [];
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
@@ -21,21 +27,53 @@ function askForName() {
 }
 
 function askQuestion() {
-  // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-  candidateAnswer = input.question(`${question}`);
+  // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswers //
+  // Replace your code with a loop that asks
+  // each question in the array and stores the user’s responses.
+
+  // this needs to go through the array and input the index of the array, corresponding to a question, in order to ask each question
+  // update the variable question to the different questions in the questions array
+
+  //   let i = 0;
+
+  // while (i < 51) {
+  //    console.log(i);
+  //    i++;
+  // }
+
+  //   for (let i = 0; i < 51; i++) {
+  //     console.log(i);
+  //  }
+
+  for (let i = 0; i < questions.length; i++) {
+    let answer = input.question(`${questions[i]}`);
+    candidateAnswers.push(answer);
+  }
+
+  //   let name = "LaunchCode";
+
+  // for (let i = 0; i < name.length; i++) {
+  //    console.log(name[i]);
+  // }
+
+  // candidateAnswer = input.question(`${question}`);
 }
 
 function gradeQuiz(candidateAnswers) {
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly //
-
-  if (candidateAnswer == "Sally Ride") {
-    console.log("Congrats! That answer is correct!");
-  } else if (candidateAnswer != "Sally Ride") {
-    console.log("That answer is incorrect.");
+  console.log(
+    `Your answers were ${candidateAnswers} and the correct answers were ${correctAnswers}`
+  );
+  let grade = 0;
+  for (let i = 0; i < questions.length; i++) {
+    if (correctAnswers[i].toUpperCase() === candidateAnswers[i].toUpperCase()) {
+      console.log("Your answer is correct!");
+      grade = grade + 20;
+    } else {
+      console.log("Your answer is not correct!");
+    }
   }
-
-  let grade; //TODO 3.2 use this variable to calculate the candidates score.
-
+  console.log(`Your score is: ${grade}`);
   return grade;
 }
 
